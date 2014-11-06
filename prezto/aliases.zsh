42815088937c2154aec5b33155a53c5d99a35346
Josh Symonds <veraticus@gmail.com>
2014-11-06 16:09:54 +0100

    A couple small improvements

---
+++ prezto/aliases.zsh
@@ -0,0 +1,5 @@
+# Aliases
+alias rake="rbbe rake"
+alias cap="rbbe cap"
+alias knife="rbbe knife"
+alias librarian-chef="rbbe librarian-chef"
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -22,7 +22,6 @@ setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-setopt AUTO_CD
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
@@ -34,9 +33,6 @@ setopt AUTO_CD
 
 # Now we can pipe to multiple outputs!
 setopt MULTIOS
-
-# Spell check commands!  (Sometimes annoying)
-setopt CORRECT
 
 # This makes cd=pushd
 setopt AUTO_PUSHD
@@ -51,12 +47,17 @@ setopt PUSHD_MINUS
 # blank pushd goes to home
 setopt PUSHD_TO_HOME
 
+# this will ignore multiple directories for the stack.  Useful?  I dunno.
+setopt PUSHD_IGNORE_DUPS
+
 # 10 second wait if you do something that will delete everything.  I wish I'd had this before...
 setopt RM_STAR_WAIT
 
 # use magic (this is default, but it can't hurt!)
 setopt ZLE
+
 setopt NO_HUP
+
 setopt IGNORE_EOF
 
 # If I could disable Ctrl-s completely I would!
@@ -77,69 +78,6 @@ setopt EXTENDED_GLOB
 # hows about arrays be awesome?  (that is, frew${cool}frew has frew surrounding all the variables, not just first and last
 setopt RC_EXPAND_PARAM
 
-#}}}
-
-#{{{ Completion Stuff
-
-bindkey -M viins '\C-i' complete-word
-
-# Faster! (?)
-zstyle ':completion::complete:*' use-cache 1
-
-# case insensitive completion
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*:descriptions' format '%B%d%b'
-zstyle ':completion:*:messages' format '%d'
-zstyle ':completion:*:warnings' format 'No matches for: %d'
-zstyle ':completion:*' group-name ''
-#zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete
-zstyle ':completion:*' completer _expand _force_rehash _complete _approximate _ignored
-
-# generate descriptions with magic.
-zstyle ':completion:*' auto-description 'specify: %d'
-
-# Don't prompt for a huge list, page it!
-zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-
-# Don't prompt for a huge list, menu it!
-zstyle ':completion:*:default' menu 'select=0'
-
-# Have the newer files last so I see them first
-zstyle ':completion:*' file-sort modification reverse
-
-# color code completion!!!!  Wohoo!
-zstyle ':completion:*' list-colors "=(#b) #([0-9]#)*=36=31"
-
-unsetopt LIST_AMBIGUOUS
-setopt  COMPLETE_IN_WORD
-
-# Separate man page sections.  Neat.
-zstyle ':completion:*:manuals' separate-sections true
-
-# complete with a menu for xwindow ids
-zstyle ':completion:*:windows' menu on=0
-zstyle ':completion:*:expand:*' tag-order all-expansions
-
-# more errors allowed for large words and fewer for small words
-zstyle ':completion:*:approximate:*' max-errors 'reply=(  $((  ($#PREFIX+$#SUFFIX)/3  ))  )'
-
-# Errors format
-zstyle ':completion:*:corrections' format '%B%d (errors %e)%b'
-
-# Don't complete stuff already on the line
-zstyle ':completion::*:(rm|vi):*' ignore-line true
-
-# Don't complete directory we are already in (../here)
-zstyle ':completion:*' ignore-parents parent pwd
-
-zstyle ':completion::approximate*:*' prefix-needed false
-
-#}}}
-
-#{{{ Key bindings
-
 # Who doesn't want home and end to work?
 bindkey '\e[1~' beginning-of-line
 bindkey '\e[4~' end-of-line
@@ -157,77 +95,8 @@ bindkey "\eOP" run-help
 # oh wow!  This is killer...  try it!
 bindkey -M vicmd "q" push-line
 
-# Rebind the insert key.  I really can't stand what it currently does.
-bindkey '\e[2~' overwrite-mode
-
-# Rebind the delete key. Again, useless.
-bindkey '\e[3~' delete-char
-
-bindkey -M vicmd '!' edit-command-output
-
 # it's like, space AND completion.  Gnarlbot.
 bindkey -M viins ' ' magic-space
-
-#}}}
-
-#{{{ History Stuff
-
-# Where it gets saved
-HISTFILE=~/.history
-
-# Remember about a years worth of history (AWESOME)
-SAVEHIST=100000
-HISTSIZE=100000
-
-# Write after each command
-setopt INC_APPEND_HISTORY
-
-# Killer: share history between multiple shells
-setopt SHARE_HISTORY
-
-# If I type cd and then cd again, only save the last one
-setopt HIST_IGNORE_DUPS
-
-# Even if there are commands inbetween commands that are the same, still only save the last one
-setopt HIST_IGNORE_ALL_DUPS
-
-# Pretty Obvious.  Right?
-setopt HIST_REDUCE_BLANKS
-
-# If a line starts with a space, don't save it.
-setopt HIST_IGNORE_SPACE
-setopt HIST_NO_STORE
-
-# When using a hist thing, make a newline show the change before executing it.
-setopt HIST_VERIFY
-
-# Save the time and how long a command ran
-setopt EXTENDED_HISTORY
-
-setopt HIST_SAVE_NO_DUPS
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_FIND_NO_DUPS
-
-#}}}
-
-_force_rehash() {
-  (( CURRENT == 1 )) && rehash
-  return 1  # Because we didn't really complete anything
-}
-
-#}}}
-
-#{{{ ZSH Modules
-
-autoload -U compinit promptinit zcalc zsh-mime-setup
-compinit
-promptinit
-zsh-mime-setup
-
-#}}}
-
-alias rake="noglob rake"
-alias simulator="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/De‌​veloper/Applications/iPhone\ Simulator.app"
 
 zle -N newtab
 
