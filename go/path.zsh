a556fd233bfc50208f40f50572373337d81b370f
Josh Symonds <veraticus@gmail.com>
2017-06-30 11:03:38 -0500

    Better detect go path

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,5 @@
-export GOPATH=$PROJECTS/go
-export PATH=$PATH:$(go env GOPATH)/bin
+go_path=$(which go)
+if [[ -x ${go_path} ]]; then
+  export GOPATH=$PROJECTS/go
+  export PATH=$PATH:$(go env GOPATH)/bin
+fi
